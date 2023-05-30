26590b7c25716e5a09a0354105e552b7d6b50134
marvin blunck <marvinblunck@gmail.com>
2023-05-30 13:19:24 -0700

    wrote the files properly and js just needs to be prompted

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 // Assignment code here
-var characterLength = 8;
-var arraySelect= []
+function generatePassword(){
+
 // Need the following variables:
     // selected length of the password
     // array of lowercase letters
@@ -14,13 +14,49 @@ var arraySelect= []
       var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
       var numbers = ["0", "1", "2", "3","4", "5", "6", "7", "8", "9"];
       var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
-      
-       
+      var arraySelect= [];
+      var passwordLength= getPasswordLength();
 
   // prompt for password length --> stored in a variable
-  var passwordLength= getpasswordLength();
+  // chat gpt helped write this code more efficiently 
+ do{
+  var lowCase= getSelect("lowercase");
+  var upperCase= getSelect("uppercase");
+  var numeric= getSelect("number");
+  var specialChar= getSelect("special");
+  
+  if (confirm("Would you like uppercase letters in your password?")){
+    arraySelect=arraySelect.concat(uppercase);
+    }
+    // prompt --> Do they want lowercase letters?
+    if (confirm("Would you like lowercase letters in your password?")){
+    arraySelect=arraySelect.concat(lowercase);
+    }
+    // prompt --> Do they want numbers?
+    if (confirm("Would you like numbers letters in your password?")){
+    arraySelect=arraySelect.concat(numbers);
+    }
+    // prompt --> Do they want special characters?
+    if (confirm("Would you like special letters in your password?")){
+    arraySelect=arraySelect.concat(special);
+    }
+    if(arraySelect.length > 0){
+      break;
+    }else {
+      alert("select at least one character type");
+    }
+ } while (true);
+
+ var password = "";
+ for (var i =0; i <passwordLength; i++){
+   password += arraySelect[Math.random()*(arraySelect.length)]
+ }
+ return password;
+}
   // Validate the user input --> if user chooses <8 or >128, alert that they need to choose a valid password length, then they're going to need to restart OR call the function that prompts for length again
+  
   function getPasswordLength(){
+    var characterLength= 0;
     characterLength = parseInt(prompt("How many characters do you want your password to be? 8-128?"));
 
     if(isNan(characterLength) || characterLength < 8 ||characterLength > 128) {
@@ -29,35 +65,33 @@ var arraySelect= []
     }
   }
   
-  // prompt --> Do they want uppercase letters?
-  if (confirm("Would you like uppercase letters in your password?")){
-  arraySelect=arraySelect.concat(uppercase);
+  //
+    
+  //chat gpt helped me write this user select function that prompts the password.
+  function getSelect(currentChoice) {
+    var messagePrompt = 'Would you like ' + currentChoice + ' characters (yes/no)?';
+  
+    while (true) {
+      var userSelect = prompt(messagePrompt);
+  
+      if (userSelect === null) {
+        // Handle the case where the user cancels the prompt
+        return false;
+      }
+  
+      userSelect = userSelect.toLowerCase();
+  
+      if (userSelect === 'yes') {
+        return true;
+      } else if (userSelect === 'no') {
+        return false;
+      } else {
+        // Prompt again if the user enters an invalid response
+        alert('Invalid input. Please enter "yes" or "no".');
+      }
+    }
   }
-  // prompt --> Do they want lowercase letters?
-  if (confirm("Would you like lowercase letters in your password?")){
-  arraySelect=arraySelect.concat(lowercase);
-  }
-  // prompt --> Do they want numbers?
-  if (confirm("Would you like numbers letters in your password?")){
-  arraySelect=arraySelect.concat(numbers);
-  }
-  // prompt --> Do they want special characters?
-  if (confirm("Would you like special letters in your password?")){
-  arraySelect=arraySelect.concat(special);
-  }
-  return true;
-
-  // Validate that the user has chosen at least one character set --> if not, either have them start over OR recursively call the function that prompts for the character sets
-  // With each of those prompts, you need an array of those character types
-    // There needs to be at least one of each selected character type included in the password
-    // Randomly generate a character from each character set as it's chosen and add it to the end of the password that you're building (.push() for an array or .concat() for a string)
-    // for loop:
-      // for (var i = password.length - 1; i < passwordLength; i++)
-    // EITHER keep the character sets separate OR combine them to a mega-array to pull the rest of the characters from
-      // IF keeping the character sets separate, randomly generate a number to tell which array you're pulling from, then randomly generate a number to pull the character from the array (character will by array[number])
-      // IF pushing to a mega-array randomly generate a number to pull a character from the array
-      // IF pushing to a mega-array, remember to set the array back to an empty array at the start of the process!
-  // return the generated password
+    
 
 
 // Get references to the #generate element
